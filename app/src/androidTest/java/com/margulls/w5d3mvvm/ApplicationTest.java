f4eef859d9a8bbfd8fb952317fd61edecb144dfe
eguegan <e.guegan@margulls.com>
2016-05-04 16:58:39 -0400

    first commit

---
+++ app/src/androidTest/java/com/margulls/w5d3mvvm/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.margulls.w5d3mvvm;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
